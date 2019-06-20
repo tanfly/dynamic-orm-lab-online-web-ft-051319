b1ecbca35cf778e1da9175fb43185faa65ce708c
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 21:28:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,8 +51,8 @@ class InteractiveRecord
   end
   
   def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-  DB[:conn].execute(sql, name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
   
   def self.find_by(attribute)
