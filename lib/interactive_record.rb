cc7eaac58f939846c79dd4496c8958a2b367cbd2
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 21:35:20 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,45 +5,48 @@ class InteractiveRecord
   
   def self.table_name
     self.to_s.downcase.pluralize
-  end
-  
+  end 
+
+
    def self.column_names
-    DB[:conn].results_as_hash = true
+    DB[:conn].results_as_hash = true 
 
-
-    sql = "PRAGMA table_info('#{table_name}')"
-
-
+     sql = "PRAGMA table_info ('#{table_name}')"
     table_info = DB[:conn].execute(sql)
-    
-   table_info.map do |column| 
+
+     table_info.map do |column| 
       column["name"]
     end.compact
   end 
-  
-  def initialize(attributes_hash={})
-    attributes_hash.each do |key, value|
-      self.send("#{key}=", value)
-    end
-  end
-  
-  def table_name_for_insert
-    self.class.table_name
-  end
-  
-  def values_for_insert
+
+
+   def initialize(attributes = {})
+    attributes.each do |k, v|
+      self.send("#{k}=", v)
+    end 
+  end 
+
+
+   def table_name_for_insert 
+    self.class.table_name 
+  end 
+
+
+   def col_names_for_insert 
+    self.class.column_names.delete_if {|col_name| col_name == "id"}.join(", ")
+  end 
+
+
+   def values_for_insert 
     values = []
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     values.join(", ")
-  end
-  
-   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
-  
-  def save 
+  end 
+
+
+   def save 
     sql = <<-SQL
       INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
       VALUES (#{values_for_insert})
@@ -51,17 +54,18 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
-  
-  def self.find_by_name(name)
+
+
+   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
-  end
-  
-  def self.find_by(attribute)
+  end 
+
+
+   def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = ?"
     DB[:conn].execute(sql, attribute.values.first)
   end 
-
     
 
   
