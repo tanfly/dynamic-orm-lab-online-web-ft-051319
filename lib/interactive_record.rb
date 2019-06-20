37a95dd351f4ddfa28b6467ae418a7f2efb6b2a1
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 21:18:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -32,6 +32,12 @@ class InteractiveRecord
     self.class.table_name
   end
   
-  
+    def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
   
 end
